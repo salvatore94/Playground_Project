c6023f3d14c56271d239686b86297d22eaac509a
Salvatore Polito <thedarkhacker94@gmail.com>
2017-05-26 16:06:46 +0200

    Initial Commit

---
+++ Playground_TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Playground_TestProject
+//
+//  Created by Salvatore  Polito on 26/05/17.
+//  Copyright © 2017 Salvatore  Polito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
